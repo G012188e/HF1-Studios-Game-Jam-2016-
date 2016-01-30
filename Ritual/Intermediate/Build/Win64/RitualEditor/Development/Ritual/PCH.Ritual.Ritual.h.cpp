482aa53d845b1743c1888a9b73de808800e8f8ed
gowansb@rocketmail.com <g012188e@student.staffs.ac.uk>
2016-01-30 13:56:32 +0000

    Delete old proj

--- Ritual/Intermediate/Build/Win64/RitualEditor/Development/Ritual/PCH.Ritual.Ritual.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\Ashley\Documents\GitHub\HF1_Studios_Game_Jam_2016\Ritual\Source\Ritual\Public\Ritual.h"
--- Ritual/Intermediate/Build/Win64/RitualEditor/Development/Ritual/Ritual.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_RITUAL_H__
-#define __AUTO_RITUAL_H__
-//Last Write: 29/01/2016 19:05:45 +00:00
-#include "C:\Users\Ashley\Documents\GitHub\HF1_Studios_Game_Jam_2016\Ritual\Source\Ritual\Public\Ritual.h"
-#endif//__AUTO_RITUAL_H__
